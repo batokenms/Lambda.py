fc9710397a80dc0afe416c72ad4f8fc2fd8d2af0
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-03-10 16:30:36 -0500

    Update Lambda_Function_Start.py

--- Lambda_Function_Start.py
+++ Lambda_Function_Start.py
@@ -6,6 +6,8 @@ ec2 = boto3.client('ec2')
 def is_weekday_and_time(date):
     if date.weekday() >= 5:  # Saturday = 5, Sunday = 6
         return False
+    #In Python, datetime.time(21, 0) represents the time 21:00 in 24-hour format (i.e., 9:00 PM).
+    #The datetime.time class is part of the Python datetime module and represents time without a date component. It takes three arguments - hour, minute, and second - and can be used to represent any time of the day. In this case, the hour is 21 (i.e., 9 PM), and the minute is 0.
     start_time = datetime.time(6, 0)
     end_time = datetime.time(21, 0)
     instance_time = date.time()
@@ -17,7 +19,13 @@ def lambda_handler(event, context):
         {'Name': 'tag:Schedule', 'Values': ['6-21']},
         {'Name': 'instance-state-name', 'Values': ['stopped']}
     ])['Reservations']
-
+    
+# The command above is using the describe_instances method of the ec2 object in the AWS SDK for Python (Boto3) to retrieve information about instances that meet the following criteria:
+# They have a tag with the key Schedule and value 6-21.
+# They are in a stopped state.
+# The method returns a dictionary containing information about the instances that match these criteria, and the code then selects the Reservations field from that dictionary. 
+# The Reservations field contains a list of Reservation objects, where each Reservation object represents a set of instances that were launched together.    
+    
     # Start each stopped instance that matches the filter
     for reservation in stopped_instances:
         for instance in reservation['Instances']:
